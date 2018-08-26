3931d50831e464c5ae46884b2db1911db17fe277
Roman Garanin <roman.m.garanin@gmail.com>
2018-08-26 21:09:30 +0300

    handle user input, treat as number

--- bulls_and_cows_game/src/main.rs
+++ bulls_and_cows_game/src/main.rs
@@ -1,26 +1,53 @@
 extern crate rand;
 
+use std::io;
 use rand::Rng;
 
 #[derive(Debug)]
 struct Number(char, char, char, char);
+
+impl Number {
+    fn from_correct_string(input_string: String) -> Number {
+        let chars: Vec<char> = input_string.chars().collect();
+        Number(chars[0], chars[1], chars[2], chars[3])
+    }
+}
 
 fn main() {
     println!("Bulls and cows game");
     println!("-------------------");
 
     let secret = generate_secret_number();
-    println!("{:?}", secret)
+
+    loop {
+        println!("Please input your 4 number guess");
+
+        let guess = read_input();
+        if guess.len() != 4 {
+            continue;
+        }
+
+        let input_number = Number::from_correct_string(guess);
+        println!("input: {:?}", input_number)
+    }
+}
+
+fn read_input() -> String {
+    let mut guess = String::new();
+    io::stdin().read_line(&mut guess)
+        .expect("Failed to read used input line");
+
+    String::from(guess.trim())
 }
 
 fn generate_secret_number() -> Number {
     let digits = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0'];
-    let used_digits: &mut Vec<char> = &mut Vec::new();
+    let mut used_digits: Vec<char> = Vec::new();
 
-    let first_digit = generate_digit(&digits, used_digits);
-    let second_digit = generate_digit(&digits, used_digits);
-    let third_digit = generate_digit(&digits, used_digits);
-    let fourth_digit = generate_digit(&digits, used_digits);
+    let first_digit = generate_digit(&digits, &mut used_digits);
+    let second_digit = generate_digit(&digits, &mut used_digits);
+    let third_digit = generate_digit(&digits, &mut used_digits);
+    let fourth_digit = generate_digit(&digits, &mut used_digits);
 
     Number(first_digit, second_digit, third_digit, fourth_digit)
 }
